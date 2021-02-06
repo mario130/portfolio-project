5d4fc0c0be314bf753c56c7032c9cccf6bc8eb69
mario130 <mario.yonan.work@gmail.com>
2021-02-06 17:45:41 +0200

    website done

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $('.loader').fadeOut()
+  $('.left-column').css({
+    transform: 'translate(0)'
+  })
+  $('.right-column').css({
+    transform: 'translate(0)'
+  })
+
+  var skillBars = document.getElementsByClassName('myBar')
+  for(var i = 0; i < skillBars.length; i++) {
+    var finishWidth = skillBars[i].dataset.per
+    skillBars[i].style.width = finishWidth+'%'
+  }
+
+  $('.project').hover(function(){
+    $(this).find('.tags').slideDown()
+  }, function(){
+    $(this).find('.tags').slideUp()
+  })
+
+  // // canvas
+  // var myImg = document.getElementById('myImg')
+  // var canvas = document.getElementById('canvas')
+  // var ctx = canvas.getContext('2d')
+  // myImg.style.display = 'none'
+
+  
+  // var imageOfMe = new Image();
+  // imageOfMe.src = "images/Me.png"
+  
+  // imageOfMe.onload = function(){
+  //   canvas.height = imageOfMe.height;
+  //   canvas.width = imageOfMe.width;
+  //   ctx.drawImage(imageOfMe,0,0)
+    
+  // }
+
+})
